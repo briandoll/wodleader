89616de722dd21a21570a28c2e27063946cf2a5d
Brian Doll <brian@emphaticsolutions.com>
2011-10-14 18:02:09 -0700

    allow events to be added after athetes are added

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,8 +6,22 @@ class Event < ActiveRecord::Base
   validates_presence_of :description
   validates_presence_of :competition_id
   
+  after_create :create_event_athletes
+  
   def ranked_results
     EventAthlete.ranked_and_categorized(self.id)
+  end
+
+  private
+  
+  def create_event_athletes
+    competition.athletes.each do |athlete|
+      ea = EventAthlete.find_by_athlete_id_and_event_id(athlete.id, self.id)
+      if !ea
+        ea = EventAthlete.new(:athlete_id => athlete.id, :event_id => self.id)
+        ea.save
+      end
+    end
   end
 
 end
